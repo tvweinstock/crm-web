be011e973502d8a91496872f0559d85dd6680ee7
Tobi Weinstock <tv.weinstock@gmail.com>
2014-08-19 16:21:21 -0400

    contact.rb and rolodex.rb created

--- crm.rb
+++ crm.rb
@@ -1,4 +1,4 @@
-# require_relative 'contact'
+require_relative 'contact'
 require 'sinatra'
 
 get '/' do
@@ -6,8 +6,16 @@ get '/' do
   erb :index
 end
 
-get '/contacts.new' do
+get '/contacts/new' do
   erb :new
+end
+
+get "/contacts" do
+  @contacts = []
+  @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor") 
+  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
+  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
+  erb :contacts
 end
 
 
