18fd23f85613dca9042bc4b5454713bc31622e34
Tobi Weinstock <tv.weinstock@gmail.com>
2014-08-19 23:08:56 -0400

    committing working CRM for pt 1 assignment

--- crm.rb
+++ crm.rb
@@ -1,22 +1,25 @@
-require_relative 'contact'
 require 'sinatra'
+require_relative 'contact'
+require_relative 'rolodex'
+
+@@rolodex = Rolodex.new
 
 get '/' do
   @crm_app_name = "Tobi's CRM"
   erb :index
 end
 
-get '/contacts/new' do
-  erb :new
-end
 
 get "/contacts" do
-  @contacts = []
-  @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor") 
-  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
-  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
   erb :contacts
 end
 
+get "/contacts/new" do
+  erb :new_contact
+end
 
-
+post '/contacts' do
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  @@rolodex.add_contact(new_contact)
+  redirect to('/contacts')
+end
