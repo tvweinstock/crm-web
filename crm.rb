380a6ab54b84b5c3d2a021b2806dfaeee80c1101
Tobi Weinstock <tv.weinstock@gmail.com>
2014-08-21 13:37:53 -0400

    show contact method added to crm via the rolodex

--- crm.rb
+++ crm.rb
@@ -1,11 +1,25 @@
 require 'sinatra'
 require 'data_mapper'
-require_relative 'contact'
 require_relative 'rolodex'
 
 DataMapper.setup(:default, "sqlite3:database.sqlite3")
 
+class Contact
+  include DataMapper::Resource
+
+  attr_accessor :id, :first_name, :last_name, :email, :note
+
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+  end
+end
+
+
 @@rolodex = Rolodex.new
+@@rolodex.add_contact(Contact.new('Johnny', 'Bravo', 'johnny@gmail.com', 'Rockstar'))
 
 get '/' do
   @crm_app_name = "Tobi's CRM"
@@ -21,8 +35,13 @@ get "/contacts/new" do
   erb :new_contact
 end
 
+get "/contacts/1000" do
+  @contact = @@rolodex.find(1000)
+  erb :show_contact
+end
+
 post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-  @@rolodex.add_contact(new_contact)
+  contact = @@rolodex.find(1000)
   redirect to('/contacts')
 end
--- rolodex.rb
+++ rolodex.rb
@@ -11,4 +11,8 @@ class Rolodex
     @contacts << contact
     @id += 1
   end
+
+  def find(contact_id)
+    @contacts.find { |contact| contact.id == contact_id }
+  end
 end
