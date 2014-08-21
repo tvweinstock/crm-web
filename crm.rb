d09a949263477f48d84ad7805ad6d5665d123eac
Tobi Weinstock <tv.weinstock@gmail.com>
2014-08-21 16:24:53 -0400

    edit & delete methods created

--- crm.rb
+++ crm.rb
@@ -19,7 +19,6 @@ end
 
 
 @@rolodex = Rolodex.new
-@@rolodex.add_contact(Contact.new('Johnny', 'Bravo', 'johnny@gmail.com', 'Rockstar'))
 
 get '/' do
   @crm_app_name = "Tobi's CRM"
@@ -31,17 +30,46 @@ get "/contacts" do
   erb :contacts
 end
 
+post '/contacts' do
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  @@rolodex.add_contact(new_contact)
+  redirect to('/contacts')
+end
+
+
 get "/contacts/new" do
   erb :new_contact
 end
 
-get "/contacts/1000" do
-  @contact = @@rolodex.find(1000)
-  erb :show_contact
+get "/contacts/:id" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
 
-post '/contacts' do
-  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-  contact = @@rolodex.find(1000)
-  redirect to('/contacts')
+get "/contacts/:id/edit" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
+
+put "/contacts/:id" do
+  @contact = @@rolodex.find(params[:id].to_i)
+    if @contact
+      @contact.first_name = params[:first_name]
+      @contact.last_name = params[:last_name]
+      @contact.email = params[:email]
+      @contact.note = params[:note]
+
+      redirect to ("/contacts")
+    else
+      raise Sinatra::NotFound
+    end
+  end
+
